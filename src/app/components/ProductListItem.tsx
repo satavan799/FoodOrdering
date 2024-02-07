6e3919b8c2d685e93cefed3e37c2ca5bf1528b38
Itgis Company <ungdunggis.com@gmail.com>
2024-02-07 23:27:01 +0700

    Flatlist

--- src/app/components/ProductListItem.tsx
+++ src/app/components/ProductListItem.tsx
@@ -1,16 +1,20 @@
 import { StyleSheet,View,Text,Image } from 'react-native';
 import Colors from '../../constants/Colors';
-import products from '../../../assets/data/products';
-
-const ProductListItem= ({product}) => {
+import { product } from '../../types';
+export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png';
+type ProductListItemProps = {
+    product: Product;
+};
+const ProductListItem= ({product}:ProductListItemProps) => {
   return (
     <View style={styles.container}>
       <Image 
-      source={{ uri: product.image }}
+      source={{ uri: product.image || defaultPizzaImage }}
       style={styles.image}
+      resizeMode='contain'
       />
 
-      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.title}>{product.name1}</Text>
       <Text style={styles.price}>${product.price}</Text>
 
 
@@ -24,10 +28,13 @@ const styles = StyleSheet.create({
     backgroundColor:'white',
     padding: 10,
     borderRadius:20,
+    flex:1,
+    maxWidth:'50%'
   },
   image: {
     width: '100%',
     aspectRatio: 1,
+   
   },
   separator: {
     marginVertical: 30,
